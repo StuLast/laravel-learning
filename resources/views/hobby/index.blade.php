c5731da6f1e3647728777bb180f92b2c763f64a4
Stu Last <stuart@last-family.co.uk>
2022-01-30 12:10:29 +0000

    Implements auth requirements for creating, editing and deleting a hobby

--- resources/views/hobby/index.blade.php
+++ resources/views/hobby/index.blade.php
@@ -7,27 +7,31 @@
             <div class="card">
                 <div class="card-header">All The Hobbies</div>
                 <div class="card-body">
-                   <ul class="list-group">
-                       @foreach($hobbies as $hobby)
-                        <li class="list-group-item">
-                           <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby->name }}</a>
-                           <a title="Edit" class="btn btn-light btn-sm ml-2" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Edit</a>
-                            <form style="dispay:inline;" class="float-right" action="/hobby/{{$hobby->id}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <input type="submit" value="Delete" class="btn btn-small btn-outline-danger">
-                            </form>
-                        </li>
-                       @endforeach
-                   </ul>
+                    <ul class="list-group">
+                        @foreach($hobbies as $hobby)
+                                <li class="list-group-item">
+                                    <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby->name }}</a>
+                                    @auth
+                                        <a title="Edit" class="btn btn-light btn-sm ml-2" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Edit</a>
+                                        <form style="dispay:inline;" class="float-right" action="/hobby/{{$hobby->id}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="submit" value="Delete" class="btn btn-small btn-outline-danger">
+                                        </form>
+                                    @endauth
+                                </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
             <div class="mt-3">
                 {{ $hobbies->links()  }}
             </div>
-            <div class="mt-2">
-                <a href="/hobby/create" class="btn btn-success btn-sm"><i class="fas fa-plus-circle pr-2"></i>Create New Hobby</a>
-            </div>
+            @auth
+                <div class="mt-2">
+                    <a href="/hobby/create" class="btn btn-success btn-sm"><i class="fas fa-plus-circle pr-2"></i>Create New Hobby</a>
+                </div>
+            @endauth
         </div>
     </div>
 </div>
